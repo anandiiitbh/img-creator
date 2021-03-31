9480df31185b4d79fa02a134832b9d64f0a44de7
anand_kumar <61147494+anandiiitbh@users.noreply.github.com>
2021-04-01 00:32:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
 
-header("Content-type: image/png");
-$string = $_GET['text'];
-$im     = imagecreatefrompng("random.png");
-$orange = imagecolorallocate($im, 220, 210, 60);
-$px     = (imagesx($im) - 7.5 * strlen($string)) / 2;
-imagestring($im, 3, $px, 9, $string, $orange);
-imagepng($im);
-imagedestroy($im);
+echo "hi";
 
 ?>
